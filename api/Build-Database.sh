276c0c648965745a76f3e0efaa2dd414bb3d3c72
Steven Velozo <steven@velozo.com>
2022-04-11 19:46:55 -1000

    Fixing infrastructure to be more streamlined with external libraries that have multiple servers

--- api/Build-Database.sh
+++ api/Build-Database.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
-echo "Bookstore Example - Database Build Script"
+echo "StrictureDatabase Build Script"
 echo "Contact: Steven Velozo <steven@velozo.com>"
 echo ""
 echo "---"
 echo ""
 
-echo "--> Generating JSON model from DDL (with the BookStore prefix)"
-npx stricture -i ./model/ddl/BookStore.ddl -c Compile -f ./model/json_schema/ -o BookStore
+echo "--> Generating JSON model from DDL"
+npx stricture -i ./model/ddl/Model.ddl -c Compile -f ./model/json_schema/ -o Model
 
 echo "--> Generating MySQL Create Statements"
-npx stricture -i ./model/json_schema/BookStore-Extended.json -c MySQL -f ./model/sql_create/ -o "BookStore-CreateDatabase"
+npx stricture -i ./model/json_schema/Model-Extended.json -c MySQL -f ./model/sql_create/ -o "Model-CreateDatabase"
 
 echo "--> Generating Meadow Schemas"
-npx stricture -i ./model/json_schema/BookStore-Extended.json -c Meadow -f ./model/meadow_schema/ -o "BookStore-MeadowSchema-"
+npx stricture -i ./model/json_schema/Model-Extended.json -c Meadow -f ./model/meadow/ -o "MeadowSchema-"
 
 echo "--> Generating Documentation"
-npx stricture -i ./model/json_schema/BookStore-Extended.json -c RelationshipsFull -g -f ./model/generated_diagram/
-npx stricture -i ./model/json_schema/BookStore-Extended.json -c Documentation -g -f ./model/generated_documentation/
+npx stricture -i ./model/json_schema/Model-Extended.json -c Documentation -g -f ./model/doc/
+npx stricture -i ./model/json_schema/Model-Extended.json -c Relationships -g -f ./model/doc/diagram
+npx stricture -i ./model/json_schema/Model-Extended.json -c RelationshipsFull -g -f ./model/doc/diagram/full
 
 echo "--> Database Code generation and compilation complete!"
